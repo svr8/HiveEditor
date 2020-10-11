f184d7fd5752d4494b546f108b992e2c2851c04a
svr8 <shikhar.vaish90@gmail.com>
2020-10-11 21:27:08 +0530

    working on RBT

--- static/js/RBTNode.js
+++ static/js/RBTNode.js
@@ -1,7 +1,16 @@
 "use strict";
 
+const color = {
+    RED: 0,
+    BLACK: 1
+};
+
 class RBTNode {
     constructor(left, right, parent, color) {
         // TODO
     }
+
+    static get color() {
+        return color;
+    } 
 }
